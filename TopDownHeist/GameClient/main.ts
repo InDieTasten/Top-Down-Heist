075cec49601b167c61bb8f54981a19f96b6907bd
InDieTasten <indietasten@gmail.com>
2018-05-30 23:42:40 +0200

    Add webpack build system

---
+++ TopDownHeist/GameClient/main.ts
@@ -0,0 +1,25 @@
+﻿import { HubConnectionBuilder, LogLevel } from "@aspnet/signalr";
+
+(function () {
+
+    const connection = new HubConnectionBuilder()
+        .withUrl("/chatHub")
+        .configureLogging(LogLevel.Debug)
+        .build();
+
+    connection.start().catch(err => console.error(err.ToString()));
+
+    connection.on("ReceiveMessage", (user, message : string) => {
+        const encodedMsg = user + " says " + message;
+        const li = document.createElement("li");
+        li.textContent = encodedMsg;
+        document.getElementById("messagesList").appendChild(li);
+    });
+
+    document.getElementById("sendButton").addEventListener("click", event => {
+        const user = (<HTMLInputElement>document.getElementById("userInput")).value;
+        const message = (<HTMLInputElement>document.getElementById("messageInput")).value;
+        connection.invoke("SendMessage", user, message).catch(err => console.error(err.ToString()));
+        event.preventDefault();
+    });
+})();
---
+++ TopDownHeist/webpack.config.js
@@ -0,0 +1,14 @@
+﻿const path = require("path");
+
+module.exports = {
+    entry: './GameClient/main.ts',
+    output: {
+        path: path.resolve(__dirname, 'wwwroot/js/'),
+        filename: 'game.bundle.js'
+    },
+    module: {
+        rules: [
+            { test: /\.ts$/, use: 'ts-loader' }
+        ]
+    }
+}
