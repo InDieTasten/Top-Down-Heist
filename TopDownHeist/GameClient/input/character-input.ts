7afdc37851a9114de090bf85cb44b082523e30bf
InDieTasten <indietasten@gmail.com>
2018-06-16 13:39:49 +0200

    Add linear input abstraction

---
+++ TopDownHeist/GameClient/input/character-input.ts
@@ -0,0 +1,9 @@
+﻿import { IVectorInput } from "./abstraction/vector-input";
+import { IHeadingInput } from "./abstraction/heading-input";
+
+export class CharacterInput {
+
+    movement: IVectorInput;
+    heading: IHeadingInput;
+
+}
---
+++ TopDownHeist/GameClient/input/keyboard/keyboard-input.ts
@@ -0,0 +1,32 @@
+﻿import { ILinearInput, IAbsoluteLinearInput } from "../abstraction/linear-input";
+import { IVectorInput } from "../abstraction/vector-input";
+
+export class KeyInput implements IAbsoluteLinearInput {
+
+    private keyCode: number;
+    private pressedState: boolean;
+
+    constructor(keyCode: number) {
+        this.keyCode = keyCode;
+        this.pressedState = false;
+
+        window.addEventListener("keydown", this.keyDownEventHandler);
+        window.addEventListener("keyup", this.keyUpEventHandler);
+    }
+
+    private keyDownEventHandler(event: KeyboardEvent): void {
+        if (event.keyCode === this.keyCode) {
+            this.pressedState = true;
+        }
+    }
+    private keyUpEventHandler(event: KeyboardEvent): void {
+        if (event.keyCode === this.keyCode) {
+            this.pressedState = false;
+        }
+    }
+
+    getValue(): number {
+        // + converts boolean to 0 or 1 number
+        return +this.pressedState;
+    }
+}
