02b09c83d56f0b7f8c373910ff1c4af8a273e6bd
InDieTasten <indietasten@gmail.com>
2018-07-08 13:27:55 +0200

    Working single player lobby

---
+++ TopDownHeist/SignalR/HeistHub.cs
@@ -0,0 +1,46 @@
+﻿using Microsoft.AspNetCore.SignalR;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Logging;
+using System;
+using System.Collections.Generic;
+using System.Threading;
+using System.Threading.Tasks;
+using TopDownHeist.GameServer.Abstractions;
+
+public class HeistHub : Hub
+{
+    private readonly Random random = new Random();
+    private readonly CancellationTokenSource cancellationTokenSource = new CancellationTokenSource();
+    private readonly ILogger<HeistHub> logger;
+    private readonly IGameManager gameManager;
+
+    public HeistHub(ILogger<HeistHub> logger, IGameManager gameManager)
+    {
+        this.logger = logger;
+        this.gameManager = gameManager;
+    }
+
+    public Task Pong(string message)
+    {
+        logger.LogDebug("Pong received from {connectionId}.", Context.ConnectionId);
+        return Task.FromResult(0);
+    }
+
+    public override async Task OnConnectedAsync()
+    {
+        gameManager.CreateGameLobby(Context.ConnectionId, Context.ConnectionId, string.Empty);
+        await base.OnConnectedAsync();
+    }
+
+    public override async Task OnDisconnectedAsync(Exception exception)
+    {
+        gameManager.LeaveLobby(Context.ConnectionId);
+        await base.OnDisconnectedAsync(exception);
+    }
+
+    protected override void Dispose(bool disposing)
+    {
+        cancellationTokenSource.Cancel();
+        base.Dispose(disposing);
+    }
+}
