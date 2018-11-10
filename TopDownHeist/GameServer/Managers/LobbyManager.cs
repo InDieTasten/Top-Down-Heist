92b5daed7fb6e392cec4592936f476f0fa9160ef
InDieTasten <indietasten@gmail.com>
2018-11-11 00:35:39 +0100

    Remove unnecessary using directive

--- TopDownHeist/GameServer/Managers/LobbyManager.cs
+++ TopDownHeist/GameServer/Managers/LobbyManager.cs
@@ -1,13 +1,12 @@
-﻿using Microsoft.AspNetCore.SignalR;
-using Microsoft.Extensions.DependencyInjection;
-using Microsoft.Extensions.Logging;
-using SignalR;
-using System;
+﻿using System;
 using System.Collections.Concurrent;
-using System.Collections.Generic;
 using System.Linq;
 using System.Threading;
 using System.Threading.Tasks;
+using Microsoft.AspNetCore.SignalR;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Logging;
+using SignalR;
 using TopDownHeist.GameServer.Abstractions;
 using TopDownHeist.GameServer.Exceptions;
 
