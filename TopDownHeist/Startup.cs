c442af40ac5db88ed993e77e32dc907aa71c90bd
InDieTasten <indietasten@gmail.com>
2018-11-11 01:31:34 +0100

    Register LobbyContextAccessor as scoped service to DI

--- TopDownHeist/Startup.cs
+++ TopDownHeist/Startup.cs
@@ -7,6 +7,7 @@ using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.Http;
 using Microsoft.Extensions.DependencyInjection;
 using SignalR;
+using TopDownHeist.GameServer;
 using TopDownHeist.GameServer.Abstractions;
 using TopDownHeist.GameServer.Managers;
 
@@ -23,6 +24,7 @@ namespace TopDownHeist.Server
                 .AddRazorViewEngine();
 
             services.AddSingleton<ILobbyManager, LobbyManager>();
+            services.AddScoped<ILobbyContextAccessor, LobbyContextAccessor>();
 
             services.AddResponseCompression();
 
