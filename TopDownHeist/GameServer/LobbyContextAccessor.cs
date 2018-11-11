53338c6701ec372fdabc1ffbe85e981f96d2ec48
InDieTasten <indietasten@gmail.com>
2018-11-11 01:28:54 +0100

    Implement LobbyContextAccessor

---
+++ TopDownHeist/GameServer/LobbyContextAccessor.cs
@@ -0,0 +1,16 @@
+﻿using System.Threading;
+using TopDownHeist.GameServer.Abstractions;
+
+namespace TopDownHeist.GameServer
+{
+    internal class LobbyContextAccessor : ILobbyContextAccessor
+    {
+        private AsyncLocal<ILobbyContext> _currentLobbyContext;
+
+        public ILobbyContext LobbyContext
+        {
+            get => _currentLobbyContext.Value;
+            set => _currentLobbyContext.Value = value;
+        }
+    }
+}
