19afc90e9b168100f18908742f868fcb0e2c35d6
InDieTasten <indietasten@gmail.com>
2019-01-06 08:00:20 +0100

    Add simulation manager stub

---
+++ TopDownHeist/GameServer/Simulation/SimulationManager.cs
@@ -0,0 +1,26 @@
+﻿namespace TopDownHeist.GameServer.Simulation
+{
+    internal class SimulationManager : ISimulationManager
+    {
+        private long _currentTick = 0;
+        private readonly IEntityManager _entityManager;
+        public float TicksPerSecond { get; set; } = 30f;
+
+        public SimulationManager(IEntityManager entityManager)
+        {
+            _entityManager = entityManager;
+        }
+
+
+        public void SimulateNextTick()
+        {
+            _currentTick++;
+
+            // TODO
+            // Calculate new velocities based on accelerations
+            // Calculate provisional new positions based on velocities
+            // Detect collisions between ICollidables
+            // Add normal
+        }
+    }
+}
